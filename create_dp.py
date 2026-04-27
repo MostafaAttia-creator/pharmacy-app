b89bb030d8a596332dd740ca5c2cb1b7e96e982f
Mostafa <atiamostafa778@gmail.com>
2026-04-27 08:51:31 +0300

    update

--- create_dp.py
+++ create_dp.py
@@ -1,18 +1,14 @@
 import sqlite3
 
-# conn = sqlite3.connect(r"E:\my_files\Lectures\Level 2\2nd term\Database\DB_project\pharmacy.db")
-# cursor = conn.cursor()
+conn = sqlite3.connect(r"E:\my_files\Lectures\Level 2\2nd term\Database\DB_project\pharmacy.db")
+cursor = conn.cursor()
 
 # with open(r"E:\my_files\Lectures\Level 2\2nd term\Database\DB_project\schema.sql", "r", encoding="utf-8") as f:
 #     sql_script = f.read()
 
 # cursor.executescript(sql_script)
 
-# conn.commit()
-# conn.close()
 
-conn = sqlite3.connect(r"E:\my_files\Lectures\Level 2\2nd term\Database\DB_project\pharmacy.db")
-cursor = conn.cursor()
 
 cursor.execute("DELETE FROM Sales")
 cursor.execute("DELETE FROM sqlite_sequence WHERE name='Sales'")
@@ -22,6 +18,8 @@ cursor.execute("DELETE FROM sqlite_sequence WHERE name='Customers'")
 
 cursor.execute("DELETE FROM Sale_Items")
 cursor.execute("DELETE FROM sqlite_sequence WHERE name='Sale_Items'")
+
+cursor.execute("UPDATE Medicines SET Quantity = 100")
 
 
 conn.commit()
